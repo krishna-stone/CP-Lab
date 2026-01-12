855d369f729c1fcaa2c19b3f5cc6d77659e0ab91
krishna-stone <krishna632raj@gmail.com>
2026-01-12 12:03:42 +0000

    reverse

--- 12.01.26/reverse.c
+++ 12.01.26/reverse.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
 int main() {
-
-return 0;
+  int rnum , num;
+  printf("Enter a number :");
+  scanf("%d", &num);
+  while (num != 0) {
+  rnum = num % 10 ;
+  num = num / 10;
+  printf("%d", rnum);
+  }
+  printf("\n");
+  return 0;
 }
