dd7b56b9bc05387fb20eece47627867a036ea55b
krishna-stone <krishna632raj@gmail.com>
2026-01-12 05:27:54 +0000

    ..

---
+++ 12.01.26/Natural_Number.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+ int main() {
+int i = 0;
+while (i>=i && i<= 50){
+printf("%d \n",i);
+i++;
+}
+return 0;
+}
---
+++ 12.01.26/Sum-Even.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+ int main() {
+int sum =0 , i =1;
+while(i<=50){
+if(i%2 == 0)
+	{sum = sum + i;}
+	 i++;}
+printf("%d\n", sum);
+return 0;
+}
