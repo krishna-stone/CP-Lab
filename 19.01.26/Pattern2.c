e83f4b4e9fe2ddaeb9f7c7b83e58245a16093f11
krishna-stone <krishna632raj@gmail.com>
2026-01-19 05:25:03 +0000

    Completed

---
+++ 19.01.26/Pattern2.c
@@ -0,0 +1,27 @@
+/*
+    *
+   **
+  ***
+ ****
+*****
+*/
+
+#include <stdio.h>
+
+int main() {
+    int i, j, space;
+
+    for (i = 1; i <= 5; i++) {
+        // print spaces
+        for (space = 1; space <= 5 - i; space++) {
+            printf(" ");
+        }
+        // print stars
+        for (j = 1; j <= i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
